20291e4bd874ddd5cca389c2be5dde83f23e59dd
Fred Wong <fai.wong@aggiemail.usu.edu>
2018-09-11 13:22:35 +0800

    Pure CSS parallax implement (#1)
    Change to pure CSS parallax

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,158 +1,124 @@
-var myScroll = "";
+var content = document.getElementById("content");
+var test = document.getElementById("test");
+var head = document.getElementById("head").offsetTop;
+var scrollDistance = content.scrollHeight * 0.01;
+var fps = 1000 / 60;
+var aboutDivPos = document.getElementById("aboutDiv").offsetTop;
+var portfolioDivPos = document.getElementById("portfolioDiv").offsetTop;
+var contactDivPos = document.getElementById("contactDiv").offsetTop;
 
 function loaded(){
-  
-  $(".navbar-nav li a").click(function (event) {
-    var toggle = $(".navbar-toggle").is(":visible");
-    if (toggle) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
-  
-  $(".navbar-brand").click(function (event) {
-    var toggle1 = $(".navbar-toggle").is(":visible");
-    if (toggle1) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
-  
-  myScroll = new IScroll("#wrapper",{
-    bounce: false,
-    mouseWheel: true,
-    scrollbars: true,
-    interactiveScrollbars: true,
-    keyBindings: true,
-    momentum: false,
-    click: true,
-    probeType: 2
-  });
-  
-  $("#scroller").stellar({
-    horizontalScrolling: false,
-    verticalScrolling: true,
-    responsive: false,
-    scrollProperty: "transform",
-    positionProperty: "transform",
-    parallaxBackgrounds: true,
-    parallaxElements: false,
-    hideDistantElements: true
-  });
-  
-  initPosition();
-  
-  $(window).resize(function(){
-    // var temp = updatePosition();
-    // sessionStorage.setItem("y",temp);
-    location.reload();
-  });
 
-  myScroll.on("scroll", move);
-  myScroll.on("scrollEnd", move);
-};
+    $(".navbar-nav li a").click(function (event){
+        var toggle = $(".navbar-toggle").is(":visible");
+        if (toggle) {
+            $(".navbar-collapse").collapse('hide');
+        }
+    });
+  
+    $(".navbar-brand").click(function (event){
+        var toggle1 = $(".navbar-toggle").is(":visible");
+        if (toggle1) {
+            $(".navbar-collapse").collapse('hide');
+        }
+    });
 
-document.addEventListener("touchmove", function (e) { e.preventDefault(); }, false);
-document.addEventListener("keydown", move);
+}
 
 function scrollToHome(){
-  myScroll.scrollToElement("#homeDiv");
+    if (content.scrollTop > 0) {
+        content.scrollTop -= scrollDistance;
+        setTimeout(scrollToHome, fps);
+    }
 }
 
 function scrollToAbout(){
-	myScroll.scrollToElement("#abouthead");
+    if (content.scrollTop > aboutDivPos) {
+        content.scrollTop -= scrollDistance;
+        if (content.scrollTop <= aboutDivPos) {
+            return;
+        } else {
+            setTimeout(scrollToAbout, fps);
+        }
+    } else if (content.scrollTop < aboutDivPos) {
+        content.scrollTop += scrollDistance;
+        if (content.scrollTop >= aboutDivPos) {
+            return;
+        } else {
+            setTimeout(scrollToAbout, fps);
+        }
+    }
 }
 
 function scrollToPortfolio(){
-  myScroll.scrollToElement("#portfolioDiv");
+    if (content.scrollTop > portfolioDivPos) {
+        content.scrollTop -= scrollDistance;
+        if (content.scrollTop <= portfolioDivPos) {
+            return;
+        } else {
+            setTimeout(scrollToPortfolio, fps);
+        }
+    } else if (content.scrollTop < portfolioDivPos) {
+        content.scrollTop += scrollDistance;
+        if (content.scrollTop >= portfolioDivPos) {
+            return;
+        } else {
+            setTimeout(scrollToPortfolio, fps);
+        }
+    }
 }
 
 function scrollToContact(){
-  myScroll.scrollToElement("#contactDiv");
+    if (content.scrollTop > contactDivPos) {
+        content.scrollTop -= scrollDistance;
+        if (content.scrollTop <= contactDivPos) {
+            return;
+        } else {
+            setTimeout(scrollToContact, fps);
+        }
+    } else if (content.scrollTop < contactDivPos) {
+        content.scrollTop += scrollDistance;
+        if (content.scrollTop >= contactDivPos) {
+            return;
+        } else {
+            setTimeout(scrollToContact, fps);
+        }
+    }
 }
 
-// function updatePosition(){
-//   var homeDivBottom = $("#homeDiv").outerHeight();
-//   var aboutDivBottom = homeDivBottom + $("#abouthead").outerHeight() + $("#aboutDiv").outerHeight() + $(".skill").outerHeight();
-//   var portfolioDivBottom = aboutDivBottom + $("#portfolioDiv").outerHeight() + $(".showcase").outerHeight();
-//   var contactDivBottom = portfolioDivBottom + $("#contactDiv").outerHeight();
+function move(){
   
-//   if (-(myScroll.y) < homeDivBottom){
-//     sessionStorage.setItem("y","#homeDiv");
-//   }
-//   else if (-(myScroll.y) < aboutDivBottom){
-//     sessionStorage.setItem("y","#abouthead");
-//   }
-//   else if (-(myScroll.y) < portfolioDivBottom){
-//     sessionStorage.setItem("y","#portfolioDiv");
-//   }
-//   else{
-//     sessionStorage.setItem("y","#contactDiv");
-//   }
-
-// }
-
-function initPosition(){
-  if (sessionStorage.getItem("y")){
-    var position = sessionStorage.getItem("y");
-    myScroll.scrollToElement(position);
-  }
-}
-
-function move() {
+    if (content.scrollTop < head){
+        $("nav").css("visibility","hidden");
+    }
+    if (content.scrollTop > head){
+        $("nav").css("visibility","visible");
+    }
   
-  if (myScroll.y > -300) {
-      $("nav").css("opacity","0");
-  }
-  if (myScroll.y < -300) {
-      $("nav").css("opacity","1");
-  }
-  
-  // $(".moveFromLeft").each( function(){
-  $(".opt10").each( function(){
-    var top_of_object = $(this).offset().top;
-    var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-    // var bottom_of_window = $(window).height() * 0.95;
-    var bottom_of_window = $(window).height() * 0.8;
+    $(".opt10").each( function(){
+        var top_of_object = $(this).offset().top;
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        // var bottom_of_window = $(window).height() * 0.95;
+        var bottom_of_window = $(window).height() * 0.8;
     
-    if(bottom_of_window > top_of_object && bottom_of_object > $(window).height() * 0.25){
-    // if(bottom_of_window > top_of_object){
-      // $(this).animate({left:"0"},"slow");
-      $(this).css("opacity","1");
-    }
-    else{
-      $(this).css("opacity","0.2");
-    }
-  });
-  
-  // $(".moveFromRight").each( function(){
-  //   var top_of_object = $(this).offset().top;
-  //   var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-  //   // var bottom_of_window = $(window).height() * 0.95;
-  //   var bottom_of_window = $(window).height() * 0.8;
-  //   if(bottom_of_window > top_of_object && bottom_of_object > $(window).height() * 0.25){
-  //   // if(bottom_of_window > top_of_object){
-  //     // $(this).animate({right:"0"},"slow");
-  //     $(this).css("opacity","1");
-  //   }
-  //   else{
-  //     $(this).css("opacity","0.2");
-  //   }
-  // });
-  
-  var homeDivBottom = $("#homeDiv").outerHeight();
-  var aboutDivBottom = homeDivBottom + $("#abouthead").outerHeight() + $("#aboutDiv").outerHeight() + $(".skill").outerHeight();
-  var portfolioDivBottom = aboutDivBottom + $("#portfolioDiv").outerHeight() + $(".showcase").outerHeight();
-  var contactDivBottom = portfolioDivBottom + $("#contactDiv").outerHeight();
-  
-  if (-(myScroll.y) < homeDivBottom){
-    sessionStorage.setItem("y","#homeDiv");
-  }
-  else if (-(myScroll.y) < aboutDivBottom){
-    sessionStorage.setItem("y","#abouthead");
-  }
-  else if (-(myScroll.y) < portfolioDivBottom){
-    sessionStorage.setItem("y","#portfolioDiv");
-  }
-  else{
-    sessionStorage.setItem("y","#contactDiv");
-  }
+        if(bottom_of_window > top_of_object && bottom_of_object > $(window).height() * 0.25){
+            $(this).css("opacity","1");
+        }
+        else{
+            $(this).css("opacity","0.2");
+        }
+    });
+
 }
+
+window.onresize = function(){
+    head = document.getElementById("head").offsetTop;
+    scrollDistance = content.scrollHeight * 0.01;
+    aboutDivPos = document.getElementById("aboutDiv").offsetTop;
+    portfolioDivPos = document.getElementById("portfolioDiv").offsetTop;
+    contactDivPos = document.getElementById("contactDiv").offsetTop;
+};
+
+//function show(){
+//    test.innerHTML = "<p>aboutDiv: " + document.getElementById("aboutDiv").offsetTop + "</p><p>portfolioDiv:" + document.getElementById("portfolioDiv").offsetTop + "</p>";
+//}
